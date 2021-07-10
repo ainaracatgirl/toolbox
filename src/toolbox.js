97ea14bddabb84ce6c21038662488a8082ba5957
JanCraft's Laptop <jancraft888@gmail.com>
2021-07-10 15:21:14 +0200

    Added Dark Mode
    Default dark mode.
    
    The test file needs to use a .svg logo to change the color with the dark/light color scheme.

--- src/toolbox.js
+++ src/toolbox.js
@@ -1,10 +1,20 @@
 function tbx(sel) {
+    /** @type {HTMLElement} */
     const s = document.querySelector(sel);
     s.show = () => {
         s.style.display = 'block';
     }
     s.hide = () => {
         s.style.display = 'none';
+    }
+    s.addClass = (className) => {
+        s.classList.add(className);
+    }
+    s.removeClass = (className) => {
+        s.classList.remove(className);
+    }
+    s.toggleClass = (className) => {
+        s.classList.toggle(className);
     }
     return s;
 }
